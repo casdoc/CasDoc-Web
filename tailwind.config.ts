cddea9c7e57fb0efd54b574616ca0103bb38cfe7
劉元楷 <Yuankai_@outlook.com>
2025-02-26 17:45:25 +0800

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,20 @@
+import typography from "@tailwindcss/typography";
 import type { Config } from "tailwindcss";
 /** @type {import('tailwindcss').Config} */
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+            },
+        },
     },
-  },
-  plugins: [require("@tailwindcss/typography")],
+    plugins: [typography],
 } satisfies Config;
