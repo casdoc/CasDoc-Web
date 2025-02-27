da7afd6a01cd4d11fe251032811118a75507366e
Ateto1202 <tonysu1204@gmail.com>
2025-02-27 22:44:36 +0800

    Handle the feature of split view

--- src/app/components/doc/SplitView.tsx
+++ src/app/components/doc/SplitView.tsx
@@ -1,19 +1,49 @@
-import Editor from "./Editor";
-import Preview from "./Preview";
+import { useDocModeViewModel } from "@/app/viewModels/DocViewModel";
+import SingleDoc from "./SingleDoc";
+import { DocMode } from "@/app/models/enum/DocMode";
+import { useEffect } from "react";
+import { CgArrowsMergeAltH } from "react-icons/cg";
 
 interface SplitViewProps {
     content: string;
-    setContent: (content: string) => void;
+    setContent: (content: string | ((prev: string) => string)) => void;
+    setDocMode: (mode: DocMode) => void;
 }
 
-const SplitView = ({ content, setContent }: SplitViewProps) => {
+const SplitView = ({ content, setContent, setDocMode }: SplitViewProps) => {
+    const { mode: leftMode, setDocMode: setLeftMode } = useDocModeViewModel();
+    const { mode: rightMode, setDocMode: setRightMode } = useDocModeViewModel();
+
+    useEffect(() => {
+        setLeftMode(DocMode.Edit);
+    }, []);
+
     return (
-        <div className="flex w-full h-full">
-            <div className="w-1/2 p-4 border-r">
-                <Editor value={content} onChange={setContent} />
+        <div className="flex w-full h-full ml-8">
+            <div className="w-1/2 pl-4">
+                <SingleDoc
+                    content={content}
+                    setContent={setContent}
+                    mode={leftMode}
+                    setDocMode={setLeftMode}
+                    oppositeMode={rightMode}
+                />
             </div>
-            <div className="w-1/2 p-4">
-                <Preview content={content} />
+            <button
+                onClick={() => setDocMode(DocMode.Preview)}
+                className="fixed inset-1/2 -translate-x-1/2 -translate-y-1/2 h-fit w-fit px-3 py-1 
+                            bg-[#D9D9D9] border border-white rounded hover:bg-[#F8FAFC]"
+            >
+                <CgArrowsMergeAltH size={30} />
+            </button>
+            <div className="w-1/2 pr-4">
+                <SingleDoc
+                    content={content}
+                    setContent={setContent}
+                    mode={rightMode}
+                    setDocMode={setRightMode}
+                    oppositeMode={leftMode}
+                />
             </div>
         </div>
     );
