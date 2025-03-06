6428007414989c83f1aa464c70a75d94db0fc1a2
Yuankai619 <Yuankai_@outlook.com>
2025-03-06 17:29:31 +0800

    set tmp json data

--- src/app/components/doc/EditorView.tsx
+++ src/app/components/doc/EditorView.tsx
@@ -1,33 +1,36 @@
 import { BlockViewModel } from "@/app/viewModels/BlockViewModel";
 import { useEditor, EditorContent } from "@tiptap/react";
 import { ExtensionKit } from "@/extensions/ExtensionKit";
-
+import tmpData from "./tmp.json";
 export interface EditorViewProps {
     blockViewModel: BlockViewModel;
 }
 const EditorView = ({ blockViewModel }: EditorViewProps) => {
     const editor = useEditor({
         extensions: [...ExtensionKit()],
+        editorProps: {
+            attributes: {
+                class: "prose prose-sm sm:prose-base lg:prose-lg xl:prose-2xl m-5 focus:outline-none",
+            },
+        },
         // immediatelyRender: true,
         // shouldRerenderOnTransaction: false,
         // autofocus: true,
-        content: `<h1>This is a 1st level heading</h1>
-        <h2>This is a 2nd level heading</h2>
-        <h3>This is a 3rd level heading</h3>
-        <h4>This 4th level heading will be converted to a paragraph, because levels are configured to be only 1, 2 or 3.</h4`,
+        // content: `<h1>This is a 1st level heading</h1>
+        // <h2>This is a 2nd level heading</h2>
+        // <h3>This is a 3rd level heading</h3>
+        // <h4>This 4th level heading will be converted to a paragraph, because levels are configured to be only 1, 2 or 3.</h4`,
+        content: tmpData,
     });
 
     if (!editor) {
         return null;
     }
-    // window.editor = editor;
+    console.debug(editor.getJSON());
     return (
         <div className="max-w-4xl min-h-screen bg-white rounded-lg shadow-xl py-10 px-6">
             {/* <div className="relative flex flex-col flex-1 h-full overflow-hidden border-red-400 border-2"> */}
-            <EditorContent
-                editor={editor}
-                className="prose border-red-400 border-2 h-full"
-            />
+            <EditorContent editor={editor} />
             {/* </div> */}
         </div>
     );
