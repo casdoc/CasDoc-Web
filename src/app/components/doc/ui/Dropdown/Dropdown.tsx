63018d85053d3ed57ab5d1b18a4aaac24dbd35a9
劉元楷 <Yuankai_@outlook.com>
2025-03-15 22:01:17 +0800

    fix Dropdown button style

--- src/app/components/doc/ui/Dropdown/Dropdown.tsx
+++ src/app/components/doc/ui/Dropdown/Dropdown.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/utils";
 import React from "react";
 
 export const DropdownCategoryTitle = ({
@@ -25,24 +26,16 @@ export const DropdownButton = React.forwardRef<
     { children, isActive, onClick, disabled, className },
     ref
 ) {
-    const buttonClass =
-        `flex items-center gap-2 p-1.5 text-sm font-medium text-neutral-500 dark:text-neutral-400 text-left bg-transparent w-full rounded
-        ${
-            !isActive && !disabled
-                ? "hover:bg-neutral-100 hover:text-neutral-800 dark:hover:bg-neutral-900 dark:hover:text-neutral-200"
-                : ""
-        }
-        ${
-            isActive && !disabled
-                ? "bg-neutral-100 text-neutral-800 dark:bg-neutral-900 dark:text-neutral-200"
-                : ""
-        }
-        ${
-            disabled
-                ? "text-neutral-400 cursor-not-allowed dark:text-neutral-600"
-                : ""
-        }
-        ${className || ""}`.trim();
+    const buttonClass = cn(
+        "flex items-center gap-2 p-1.5 text-sm font-medium text-neutral-500 dark:text-neutral-400 text-left bg-transparent w-full rounded",
+        !isActive && !disabled,
+        "hover:bg-neutral-100 hover:text-neutral-800 dark:hover:bg-neutral-900 dark:hover:text-neutral-200",
+        isActive &&
+            !disabled &&
+            "bg-neutral-100 text-neutral-800 dark:bg-neutral-900 dark:text-neutral-200",
+        disabled && "text-neutral-400 cursor-not-allowed dark:text-neutral-600",
+        className
+    );
 
     return (
         <button
