d11cd8a40b7165a7107a53b441badf957c1ca578
Ateto1202 <tonysu1204@gmail.com>
2025-03-18 22:39:25 +0800

    Open edit panel in the first time of click event

--- src/app/components/editPanel/EditPanelView.tsx
+++ src/app/components/editPanel/EditPanelView.tsx
@@ -7,11 +7,11 @@ import {
     GraphViewModel,
 } from "@/app/viewModels/GraphViewModel";
 import { DocumentViewModel } from "@/app/viewModels/useDocument";
-import { TextArea } from "@radix-ui/themes";
 import EditPanelHeader from "./EditPanelHeader";
 import EditPanelRelationship from "./EditPanelRelationship";
 import EditPanelFields from "./EditPanelFields";
 import { JsonObject } from "@/app/models/types/JsonObject";
+import EditPanelInfo from "./EditPanelInfo";
 
 interface EditPanelProps {
     documentViewModel: DocumentViewModel;
@@ -51,12 +51,6 @@ const EditPanelView = ({
     useEffect(() => {
         setIsMounted(true);
     }, []);
-
-    useEffect(() => {
-        if (!node) {
-            selectNode(null);
-        }
-    }, [node, selectNode]);
 
     const handleFieldChange = (
         e: React.ChangeEvent<HTMLTextAreaElement>,
@@ -123,55 +117,13 @@ const EditPanelView = ({
             }`}
         >
             <EditPanelHeader onClose={() => selectNode(null)} />
-
             {selectedNode ? (
                 <div className="mt-4 flex flex-col h-full space-y-4 overflow-auto pb-32">
-                    <div className="bg-white border border-gray-200 rounded-lg p-4 mr-4 shadow">
-                        <h2 className="text-lg font-semibold mb-4">
-                            Basic Info
-                        </h2>
-                        <p className="text-sm text-gray-500 mb-4">
-                            <span className="font-semibold">ID:</span>{" "}
-                            {selectedNode}
-                        </p>
-                        {node?.config && Object.keys(node.config).length > 0 ? (
-                            <div className="grid grid-cols-2 gap-x-4 gap-y-2">
-                                {Object.entries(node.config).map(
-                                    ([key, value]) => (
-                                        <div
-                                            key={key}
-                                            className="flex flex-col space-y-1"
-                                        >
-                                            <label
-                                                className="text-sm text-gray-600 font-medium truncate"
-                                                title={key}
-                                            >
-                                                {key}
-                                            </label>
-                                            <TextArea
-                                                size="2"
-                                                resize="none"
-                                                radius="medium"
-                                                className="resize-none bg-white p-2 text-sm w-full border border-gray-300 rounded-md focus:ring focus:ring-indigo-200"
-                                                value={
-                                                    value !== undefined
-                                                        ? String(value)
-                                                        : ""
-                                                }
-                                                onChange={(e) =>
-                                                    handleConfigChange(e, key)
-                                                }
-                                            />
-                                        </div>
-                                    )
-                                )}
-                            </div>
-                        ) : (
-                            <p className="text-gray-500 text-sm">
-                                No config fields
-                            </p>
-                        )}
-                    </div>
+                    <EditPanelInfo
+                        selectedNode={selectedNode}
+                        config={node?.config}
+                        handleConfigChange={handleConfigChange}
+                    />
                     {node?.type && node?.type.startsWith("template") && (
                         <div className="bg-white border border-gray-200 rounded-lg p-4 mr-4 shadow">
                             <h2 className="text-lg font-semibold mb-4">
