5cf2097b41da78eaeed1e50beed084e7363660e6
Ateto1202 <tonysu1204@gmail.com>
2025-03-18 10:00:31 +0800

    Fix graph node handle issue

--- src/app/components/flow/CustomNode.tsx
+++ src/app/components/flow/CustomNode.tsx
@@ -18,11 +18,13 @@ function CustomNode({ id, data }: any) {
         <button
             disabled={id === "root"}
             onClick={handleClick}
-            className={`${id === "root" && "cursor-not-allowed"}`}
+            className={`${
+                id === "root" ? "cursor-not-allowed" : "hover:opacity-70"
+            }`}
         >
             <div
                 className={`px-4 py-2 shadow-md rounded-md bg-white border-2  ${
-                    isSelected ? "border-indigo-500" : "border-stone-400"
+                    isSelected ? "border-blue-500" : "border-stone-400"
                 } ${
                     data.type.startsWith("topic")
                         ? "shadow-cyan-200"
@@ -37,6 +39,7 @@ function CustomNode({ id, data }: any) {
                     <Handle
                         id="left"
                         type="target"
+                        isConnectableStart={false}
                         position={Position.Left}
                         className="rounded-none h-5 border-0 !bg-gray-400"
                     />
@@ -45,6 +48,8 @@ function CustomNode({ id, data }: any) {
                     <Handle
                         id="right"
                         type="source"
+                        isConnectableStart={data.type.startsWith("template")}
+                        isConnectableEnd={data.type.startsWith("template")}
                         position={Position.Right}
                         className="rounded-none h-5 border-0 !bg-gray-400"
                     />
