73eccd0a10f4194468dcfb790f6e48630d481800
Ateto1202 <tonysu1204@gmail.com>
2025-04-02 21:19:54 +0800

    Enable to edit and store the edge label content

--- src/app/components/editPanel/RelationshipSection.tsx
+++ src/app/components/editPanel/RelationshipSection.tsx
@@ -5,6 +5,7 @@ import { ConnectionEdge } from "@/app/viewModels/GraphViewModel";
 import { JsonObject } from "@/app/models/types/JsonObject";
 import { FiLogIn } from "react-icons/fi";
 import { FaRegTrashAlt } from "react-icons/fa";
+import { TextArea } from "@radix-ui/themes";
 
 interface RelationshipSectionProps {
     title: string;
@@ -15,6 +16,7 @@ interface RelationshipSectionProps {
     show: boolean;
     toggleShow: () => void;
     removeEdge: (edge: ConnectionEdge) => void;
+    updLabel: (edge: ConnectionEdge, content: string) => void;
 }
 
 const RelationshipSection = ({
@@ -26,6 +28,7 @@ const RelationshipSection = ({
     show,
     toggleShow,
     removeEdge,
+    updLabel,
 }: RelationshipSectionProps) => {
     const { selectNode } = useNodeSelection();
 
@@ -59,24 +62,38 @@ const RelationshipSection = ({
                     return (
                         <div
                             key={`${edge.source}-${edge.target}`}
-                            className="flex justify-between bg-gray-100 py-2 mb-2 px-4 rounded text-sm w-full"
+                            className="flex justify-between border-t border-gray-300 py-2 mb-2 px-4 text-sm"
                         >
-                            {node.config.info.name}
-                            <div className="flex">
-                                <button
-                                    onClick={() =>
-                                        selectNode(nodeIdGetter(edge))
+                            <div className="flex flex-col w-full pb-2">
+                                <div className="flex items-center justify-between py-1">
+                                    <span className="text-md text-gray-800 font-semibold">
+                                        {node.config.info.name}
+                                    </span>
+                                    <div>
+                                        <button
+                                            onClick={() =>
+                                                selectNode(nodeIdGetter(edge))
+                                            }
+                                            className="mx-2"
+                                        >
+                                            <FiLogIn size={18} />
+                                        </button>
+                                        <button
+                                            onClick={() => handleRemove(edge)}
+                                            className="mx-2"
+                                        >
+                                            <FaRegTrashAlt size={18} />
+                                        </button>
+                                    </div>
+                                </div>
+                                <TextArea
+                                    value={edge.label}
+                                    placeholder="Describe why you connect them"
+                                    onChange={(e) =>
+                                        updLabel(edge, e.target.value)
                                     }
-                                    className="mx-2"
-                                >
-                                    <FiLogIn size={18} />
-                                </button>
-                                <button
-                                    onClick={() => handleRemove(edge)}
-                                    className="mx-2"
-                                >
-                                    <FaRegTrashAlt size={18} />
-                                </button>
+                                    className="text-sm text-gray-500 font-normal mt-2 bg-transparent resize-none focus:outline-none"
+                                />
                             </div>
                         </div>
                     );
