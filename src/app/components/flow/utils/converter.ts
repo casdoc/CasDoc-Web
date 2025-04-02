f4bc472f963107b58332aab550f33d038376920e
Ateto1202 <tonysu1204@gmail.com>
2025-04-03 07:47:29 +0800

    Use the actual type instead of using any

--- src/app/components/flow/utils/converter.ts
+++ src/app/components/flow/utils/converter.ts
@@ -1,10 +1,10 @@
-import { MarkerType, Position } from "@xyflow/react";
+import { ConnectionEdge } from "@/app/viewModels/GraphViewModel";
+import { GraphNode } from "@/app/viewModels/useDocument";
+import { Edge, MarkerType, Node, Position } from "@xyflow/react";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const convertDataToNodes = (data: any) => {
+export const convertDataToNodes = (data: GraphNode[]): Node[] => {
     const defaultPosition = { x: 0, y: 0 };
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    return data.map((item: any) => ({
+    return data.map((item: GraphNode) => ({
         id: `${item.id}`,
         position: defaultPosition,
         data: {
@@ -16,44 +16,33 @@ export const convertDataToNodes = (data: any) => {
     }));
 };
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const convertDataToStructuralEdges = (data: any) => {
-    const edges = [];
-
-    // tree structure edges
-    if (data.length > 0) {
-        for (let i = 1; i < data.length; i++) {
-            edges.push({
-                id: `e-${data[i].pid}-${data[i].id}`,
-                source: `${data[i].pid}`,
-                target: `${data[i].id}`,
-                arrowHeadType: MarkerType.ArrowClosed,
-                type: "default",
-                deletable: false,
-                selectable: false,
-            });
-        }
-    }
-    return edges;
+export const convertDataToStructuralEdges = (
+    graphNodes: GraphNode[]
+): Edge[] => {
+    return graphNodes.map((node) => {
+        return {
+            id: `e-${node.pid}-${node.id}`,
+            source: `${node.pid}`,
+            target: `${node.id}`,
+            arrowHeadType: MarkerType.ArrowClosed,
+            type: "default",
+            deletable: false,
+            selectable: false,
+        };
+    });
 };
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const connectConnectionEdges = (connectionEdges: any) => {
-    const edges = [];
-
-    // component connection edges
-    if (connectionEdges.length > 0) {
-        for (let i = 0; i < connectionEdges.length; i++) {
-            edges.push({
-                id: `e-${connectionEdges[i].source}-${connectionEdges[i].target}`,
-                source: `${connectionEdges[i].source}`,
-                target: `${connectionEdges[i].target}`,
-                arrowHeadType: MarkerType.ArrowClosed,
-                type: "custom",
-                targetHandle: Position.Right,
-                label: connectionEdges[i].label,
-            });
-        }
-    }
-    return edges;
+export const connectConnectionEdges = (
+    connectionEdges: ConnectionEdge[]
+): Edge[] => {
+    return connectionEdges.map((e) => {
+        return {
+            id: `e-${e.source}-${e.target}`,
+            source: `${e.source}`,
+            target: `${e.target}`,
+            type: "custom",
+            targetHandle: Position.Right,
+            label: e.label,
+        };
+    });
 };
