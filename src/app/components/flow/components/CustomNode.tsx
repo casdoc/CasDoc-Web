2369339c17388c452ed513ddadcfa79bff48538b
Ateto1202 <tonysu1204@gmail.com>
2025-04-06 12:42:08 +0800

    Create notif red point

--- src/app/components/flow/components/CustomNode.tsx
+++ src/app/components/flow/components/CustomNode.tsx
@@ -23,7 +23,7 @@ function CustomNode({ id, data }: any) {
             }`}
         >
             <div
-                className={`px-4 py-2 shadow-md rounded-md bg-white border-2  ${
+                className={`relative px-4 py-2 shadow-md rounded-md bg-white border-2  ${
                     isSelected ? "border-blue-500" : "border-stone-400"
                 } ${
                     data.type.startsWith("topic")
@@ -33,6 +33,9 @@ function CustomNode({ id, data }: any) {
                         : ""
                 }`}
             >
+                {data.isAffected || (
+                    <div className="absolute -top-1 -right-1.5 w-2.5 h-2.5 rounded-full bg-red-400" />
+                )}
                 <div className="max-w-44 overflow-hidden whitespace-nowrap truncate">
                     {data.label}
                 </div>
