d0f089181a99d85874727543f29de4622307f236
Ateto1202 <tonysu1204@gmail.com>
2025-04-19 21:01:44 +0800

    Create a protected user interface

--- src/pages/clean.tsx
+++ src/pages/clean.tsx
@@ -1,17 +1,48 @@
+import { Button } from "@/components/ui/button";
+import {
+    Dialog,
+    DialogHeader,
+    DialogContent,
+    DialogTitle,
+    DialogDescription,
+} from "@/components/ui/dialog";
+import { Flex } from "@radix-ui/themes";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
 
 export default function Clean() {
     const router = useRouter();
-    useEffect(() => {
-        setTimeout(() => {
-            localStorage.clear();
-            router.push("/");
-        }, 500);
-    }, [router]);
+
+    const handleClear = () => {
+        localStorage.clear();
+        router.push("/");
+    };
+
     return (
-        <div className="flex justify-center items-center h-screen w-screen text-6xl font-bold">
-            Cleaning...
-        </div>
+        <Dialog open={true}>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>
+                        Do you sure to clear the data in local storage?
+                    </DialogTitle>
+                    <DialogDescription>
+                        The operation can not be reversed.
+                    </DialogDescription>
+                </DialogHeader>
+                <Flex className="gap-x-4 mt-5">
+                    <Button
+                        onClick={() => router.push("/")}
+                        className="hover:bg-gray-400"
+                    >
+                        Cancel
+                    </Button>
+                    <Button
+                        onClick={handleClear}
+                        className="bg-gray-400 text-gray-100"
+                    >
+                        Ensure
+                    </Button>
+                </Flex>
+            </DialogContent>
+        </Dialog>
     );
 }
