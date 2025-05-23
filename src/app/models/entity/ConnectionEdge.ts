e940903c26d6e1372b34d7371e4015544aac3bd0
owen0806 <owen080614@gmail.com>
2025-05-24 01:44:46 +0800

    Create ConnectionEdge entity

---
+++ src/app/models/entity/ConnectionEdge.ts
@@ -0,0 +1,76 @@
+import { BaseEntity } from "./BaseEntity";
+
+export class ConnectionEdge extends BaseEntity {
+    private _projectId: string;
+    private _source: string;
+    private _target: string;
+    private _label: string;
+    private _offsetValue: number;
+    private _bidirectional: boolean;
+
+    constructor(
+        id: string,
+        projectId: string,
+        source: string,
+        target: string,
+        label: string,
+        offsetValue: number,
+        bidirectional: boolean
+    ) {
+        super(id);
+        this._projectId = projectId;
+        this._source = source;
+        this._target = target;
+        this._label = label;
+        this._offsetValue = offsetValue;
+        this._bidirectional = bidirectional;
+    }
+
+    get projectId(): string {
+        return this._projectId;
+    }
+
+    get source(): string {
+        return this._source;
+    }
+
+    get target(): string {
+        return this._target;
+    }
+
+    get label(): string {
+        return this._label;
+    }
+
+    get offsetValue(): number {
+        return this._offsetValue;
+    }
+
+    get bidirectional(): boolean {
+        return this._bidirectional;
+    }
+
+    set projectId(value: string) {
+        this._projectId = value;
+    }
+
+    set source(value: string) {
+        this._source = value;
+    }
+
+    set target(value: string) {
+        this._target = value;
+    }
+
+    set label(value: string) {
+        this._label = value;
+    }
+
+    set offsetValue(value: number) {
+        this._offsetValue = value;
+    }
+
+    set bidirectional(value: boolean) {
+        this._bidirectional = value;
+    }
+}
