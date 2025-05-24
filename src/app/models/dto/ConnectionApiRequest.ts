9cef02e4a188a49e56ff44c8f331a26271b6fbf2
owen0806 <owen080614@gmail.com>
2025-05-24 15:57:47 +0800

    Add ConnectionCreate, ConnectionUpdate

---
+++ src/app/models/dto/ConnectionApiRequest.ts
@@ -0,0 +1,16 @@
+import z from "zod";
+
+export const ConnectionCreateSchema = z.object({
+    sourceId: z.string(),
+    targetId: z.string(),
+    label: z.string(),
+    offsetValue: z.number(),
+    bidirectional: z.boolean(),
+});
+export type ConnectionCreate = z.infer<typeof ConnectionCreateSchema>;
+
+export const ConnectionUpdateSchema = z.object({
+    label: z.string(),
+    offsetValue: z.number(),
+});
+export type ConnectionUpdate = z.infer<typeof ConnectionUpdateSchema>;
