52f66e88a11e7422b9fbcc1787e053b7cad9b5ba
Ateto1202 <tonysu1204@gmail.com>
2025-06-15 20:44:31 +0800

    Redesign the ui and layout of the component of the api interface

--- src/extensions/Templates/APIinterface/APIinterfaceUI.tsx
+++ src/extensions/Templates/APIinterface/APIinterfaceUI.tsx
@@ -4,6 +4,7 @@ import {
     CollapsibleTrigger,
 } from "@/components/ui/collapsible";
 import { ChevronDown } from "lucide-react";
+import { Flex, Text } from "@radix-ui/themes";
 
 export interface APIinterfaceParameter {
     name: string;
@@ -44,32 +45,36 @@ const getMethodColor = (method?: string): string => {
 const renderSection = (title: string, data: APIinterfaceParameter[]) => {
     if (!data.length) return null;
     return (
-        <div className="pt-4">
-            <h3 className="font-bold text-sm text-gray-700">{title}</h3>
-            <div className="divide-y divide-gray-100">
+        <div className="py-3">
+            <h3 className="font-bold text-sm text-gray-700 py-0">{title}</h3>
+            <ul className="divide-gray-100 mt-2 px-2">
                 {data.map((field, index) => (
-                    <div key={index} className="py-2">
-                        <div className="flex justify-between">
-                            <span className="font-medium text-gray-800">
-                                {field.name}
-                            </span>
-                            <div className="text-right">
+                    <li key={index}>
+                        <Flex justify="between" align="center" pl="2">
+                            <Flex align="center" gap="2">
+                                <span className="font-medium text-gray-800">
+                                    {field.name}
+                                </span>
+                                {field.description && (
+                                    <span className="text-sm text-gray-500">
+                                        {field.description}
+                                    </span>
+                                )}
+                            </Flex>
+                            <div className="text-right pr-2">
                                 {field.required && (
-                                    <span className="text-red-500 mr-2">*</span>
+                                    <span className="text-red-500 mr-0.5">
+                                        *
+                                    </span>
                                 )}
                                 <span className="text-xs bg-gray-200 px-2 py-1 rounded">
                                     {field.type}
                                 </span>
                             </div>
-                        </div>
-                        {field.description && (
-                            <p className="text-sm text-gray-500">
-                                {field.description}
-                            </p>
-                        )}
-                    </div>
+                        </Flex>
+                    </li>
                 ))}
-            </div>
+            </ul>
         </div>
     );
 };
@@ -82,8 +87,8 @@ const APIinterfaceUI = ({
 }: APIinterfaceUIProps) => {
     return (
         <>
-            <div className="w-full h-full pt-2 pl-4 border-b rounded-sm group/chevron">
-                <div className="flex items-start pb-2 gap-1">
+            <div className="pt-2 pl-4 border-b rounded-sm group/chevron">
+                <Flex align="center" pb="2" gap="1">
                     <span
                         className={`px-2 py-1 text-xs rounded-md text-white font-bold mr-2 max-w-24 truncate ${getMethodColor(
                             info?.method
@@ -100,14 +105,17 @@ const APIinterfaceUI = ({
                     >
                         <ChevronDown className="w-4 h-4 opacity-0 group-hover/chevron:opacity-100 transition-all duration-200 group-data-[state=open]/chevron:rotate-180" />
                     </CollapsibleTrigger>
-                </div>
-                <p className="text-sm text-gray-600">{info?.description}</p>
-                <p className="text-sm font-semibold text-black py-2">
-                    End Point : {info?.endPoint}
-                </p>
+                </Flex>
+                <Flex direction="column">
+                    <Text className="text-sm text-gray-600">
+                        {info?.description}
+                    </Text>
+                    <Text className="text-sm font-semibold text-black py-2">
+                        End Point : {info?.endPoint}
+                    </Text>
+                </Flex>
             </div>
-
-            <CollapsibleContent className="ml-8 overflow-hidden pb-4">
+            <CollapsibleContent className="ml-4 overflow-hidden">
                 {renderSection("Headers", headers)}
                 {renderSection("Request Body", requestBody)}
                 {renderSection("Response Body", responseBody)}
